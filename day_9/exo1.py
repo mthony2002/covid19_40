9b78ff77c8c00eb38b9d8f52b2181e1a19166919
Marcel J. Thony <mthony2002@gmail.com>
2020-04-09 20:29:54 +0000

    day 9

---
+++ day_9/exo1.py
@@ -0,0 +1,11 @@
+
+main_characters = [
+    ("BoJack Horseman", "Will Arnett", "Horse"),
+    ("Princess Carolyn", "Amy Sedaris", "Cat"),
+    ("Diane Nguyen", "Alison Brie", "Human"),
+    ("Mr. Peanutbutter", "Paul F. Tompkins", "Dog"),
+    ("Todd Chavez", "Aaron Paul", "Human")
+]
+
+for name,actor,animal in main_characters:
+  print (f"{name} is a {animal.lower()} voiced by {actor}.")
---
+++ day_9/exo2.py
@@ -0,0 +1,9 @@
+
+data = ("John Smith", 11743, ("Computer Science", "Mathematics"))
+name,id ,(major, minor)= data 
+
+print(f"student name : {name}")
+print(f"student id number : {id}")
+print(f"major : {major}")
+print(f"minor : {minor}")
+
